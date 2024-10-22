86548db409732613855d21706a41f7c424c557c4
terafora <charlie.stone649@gmail.com>
2024-10-22 16:46:06 +0100

    Add Update and Delete handlers for users

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"DocuDefense/src/models"
 	"encoding/json"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -18,4 +19,27 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&user)
 	users[user.ID] = user
 	json.NewEncoder(w).Encode(user)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedUser models.User
+	_ = json.NewDecoder(r.Body).Decode(&updatedUser)
+
+	if _, ok := users[params["id"]]; ok {
+		users[params["id"]] = updatedUser
+		json.NewEncoder(w).Encode(updatedUser)
+	} else {
+		http.Error(w, "User not found", http.StatusNotFound)
+	}
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	if _, ok := users[params["id"]]; ok {
+		delete(users, params["id"])
+		json.NewEncoder(w).Encode(users)
+	} else {
+		http.Error(w, "User not found", http.StatusNotFound)
+	}
 }
