c6f2add816a73aad4c56717aff8a16f97abd74bb
terafora <charlie.stone649@gmail.com>
2024-10-22 16:08:30 +0100

    Create user model

---
+++ src/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	Surname   string `json:"surname"`
+	Email     string `json:"email"`
+	Birthdate string `json:"birthdate"`
+}
