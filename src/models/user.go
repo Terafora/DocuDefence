5d03db3a731a5ec4f288fcbfe0624d65abfaacb8
terafora <charlie.stone649@gmail.com>
2024-10-23 16:56:52 +0100

    Update user so multi files could be uploaded to a user

--- src/models/user.go
+++ src/models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	Surname   string `json:"surname"`
-	Email     string `json:"email"`
-	Birthdate string `json:"birthdate"`
-	FileName  string `json:"file_name"`
+	ID        string   `json:"id"`
+	FirstName string   `json:"first_name"`
+	Surname   string   `json:"surname"`
+	Email     string   `json:"email"`
+	Birthdate string   `json:"birthdate"`
+	FileNames []string `json:"file_names"`
 }
