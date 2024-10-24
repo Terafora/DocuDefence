e5354edd8fa186bb398be1da4035ecc8587fa088
terafora <charlie.stone649@gmail.com>
2024-10-24 06:00:07 +0100

    Update User model with password and add hashing functionality with bcrypt

--- src/models/user.go
+++ src/models/user.go
@@ -1,10 +1,30 @@
 package models
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID        string   `json:"id"`
 	FirstName string   `json:"first_name"`
 	Surname   string   `json:"surname"`
 	Email     string   `json:"email"`
 	Birthdate string   `json:"birthdate"`
+	Password  string   `json:"-"`
 	FileNames []string `json:"file_names"`
+}
+
+// Hashes the user's password before storing it
+func (u *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bytes)
+	return nil
+}
+
+// Compares the user's password with the given password
+func (u *User) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 }
