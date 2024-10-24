aecb390fb17e932a090bd2ded70ea824779dd508
terafora <charlie.stone649@gmail.com>
2024-10-24 06:01:48 +0100

    Add protection for potnetially destructive routes

--- src/main.go
+++ src/main.go
@@ -13,9 +13,9 @@ func main() {
 
 	r.HandleFunc("/users", handlers.GetUsers).Methods("GET")
 	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", handlers.UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", handlers.DeleteUser).Methods("DELETE")
-	r.HandleFunc("/users/{id}/upload", handlers.UploadFile).Methods("POST")
+	r.Handle("/users/{id}", handlers.BasicAuthMiddleware(http.HandlerFunc(handlers.UpdateUser))).Methods("PUT")
+	r.Handle("/users/{id}", handlers.BasicAuthMiddleware(http.HandlerFunc(handlers.DeleteUser))).Methods("DELETE")
+	r.Handle("/users/{id}/upload", handlers.BasicAuthMiddleware(http.HandlerFunc(handlers.UploadFile))).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
